30518a7315d33ced0f05b3e6872bde95bf34d904
Gustavo Rojas Valdivia <doc.gustavo.rojas.va@unifranz.edu.bo>
2025-04-10 16:47:10 -0400

    Projecto inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Saludo from './components/Saludo';
+
+function App() {
+  return (
+    <div>
+      <Saludo></Saludo>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Saludo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Saludo = () => {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
+  )
+}
+
+export default Saludo
